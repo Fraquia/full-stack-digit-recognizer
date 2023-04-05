d8b40de16e7db5e8b1b8f1f024100f8a34d0b0bb
Marcello Politi <marcellopoliti@Marcellos-MBP.homenet.telecomitalia.it>
2023-04-05 15:40:02 +0200

    flask serving

--- src/app.py
+++ src/app.py
@@ -29,4 +29,4 @@ def home():
     return 'Welcome to digit recognizer app!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True) 
